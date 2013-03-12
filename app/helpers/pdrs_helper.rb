fa8f0d3291d40a9487e6a3007d9bc85e0ac5f78f
onilaix <marco.scialino@gmail.com>
2013-03-12 16:23:34 +0100

    Update pdrs_helper.rb
    capitalize list of systems in get_system_options

--- app/helpers/pdrs_helper.rb
+++ app/helpers/pdrs_helper.rb
@@ -3,13 +3,13 @@ module PdrsHelper
    def get_system_options
     # return list of systems to be used in select-options html
     return [
-      ["-interno-",""],
+       ["-interno-",""],
       ["AMD", "AMD"],
       ["DS","DS"],
       ["DS-LITE","DS-LITE"],
       ["GW", "GW"],
-      ["Key Client", "Key Client"],
-      ["Portale SC","Portale SC"],
+      ["KEY CLIENT", "KEY CLIENT"],
+      ["PORTALE SC","PORTALE SC"],
       ["SMS", "SMS"],
       ["SSB","SSB"],
       ["TSE", "TSE"]
